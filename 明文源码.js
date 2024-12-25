c58a7ce432d0aabfa1148108acabcbf7fdc2d18b
alice5920 <alicelewis5920@gmail.com>
2024-12-25 21:01:41 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,11 +1,11 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'da9a63c0-5df9-422c-b23c-beb1bc130bb7';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
-let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
+let subConfig = "https://raw.githubusercontent.com/gtang8/ACL4SSR/master/Clash/config/ACL4SSR_Mini.ini";
 let subProtocol = 'https';
 let subEmoji = 'true';
 let socks5Address = '';
